284639676281838da408ff1d23ccb462ab2677e2
Jamie Catto <jamietcatto@gmail.com>
2023-02-14 15:10:31 +0100

    Added empty navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import '../styles/app.css';
+import React from 'react';
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Surreal Estate</h2>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h2>Surreal Estate</h2>
+    <NavBar />
+  </div>
+);
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+const NavBar = () => (
+  <div className="navbar" />
+);
+
+export default NavBar;
