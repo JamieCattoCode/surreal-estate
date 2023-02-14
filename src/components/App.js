ce599cd0517e9f24ea7cea6f9f969008fc5c8adb
Jamie Catto <jamietcatto@gmail.com>
2023-02-14 15:32:07 +0100

    Added some items to the NavBar.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import NavBar from './NavBar';
 
 const App = () => (
   <div className="App">
-    <h2>Surreal Estate</h2>
     <NavBar />
   </div>
 );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from 'react';
 import '../styles/navbar.css';
 
 const NavBar = () => (
-  <div className="navbar" />
+  <div className="navbar">
+    <img
+      alt="Surreal Estate Logo"
+      src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
+    />
+    <ul className="navbar-list">
+      <li className="navbar-list-item">View Properties</li>
+      <li className="navbar-list-item">Add a Property</li>
+    </ul>
+  </div>
 );
 
 export default NavBar;
