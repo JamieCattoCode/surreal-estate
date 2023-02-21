bcdce2dd17cbac2a9633828b3e7bab4c743b3d97
Jamie Catto <jamietcatto@gmail.com>
2023-02-21 10:20:00 +0100

    Added Font Awesome icons to PropertyCard component.

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faShower } from '@fortawesome/free-solid-svg-icons';
 import '../styles/property-card.css';
 
 const PropertyCard = ({
   title, type, bedrooms, bathrooms, price, city, email,
-}) => (
-  <div className="property-card">
-    <div className="property-card__title">
-      {title}
+}) => {
+  const emailLink = `mailto:${email}`;
+  return (
+    <div className="property-card">
+      <div className="property-card__title">
+        {title}
+      </div>
+      <div className="property-card__type-city">
+        {type}
+        {' '}
+        -
+        {' '}
+        {city}
+      </div>
+      <div className="property-card__bedrooms">
+        <FontAwesomeIcon icon={faBed} />
+        {bedrooms}
+        {' '}
+        bedrooms
+      </div>
+      <div className="property-card__bathrooms">
+        <FontAwesomeIcon icon={faShower} />
+        {bathrooms}
+        {' '}
+        bathrooms
+      </div>
+      <div className="property-card__price">
+        £
+        {price}
+      </div>
+      <div className="property-card__email">
+        <a href={emailLink}>Contact</a>
+      </div>
     </div>
-    <div className="property-card__type-city">
-      {type}
-      {' '}
-      -
-      {' '}
-      {city}
-    </div>
-    <div className="property-card__bedrooms">
-      {bedrooms}
-      {' '}
-      bedrooms
-    </div>
-    <div className="property-card__bathrooms">
-      {bathrooms}
-      {' '}
-      bathrooms
-    </div>
-    <div className="property-card__price">
-      £
-      {price}
-    </div>
-    <div className="property-card__email">
-      {email}
-    </div>
-  </div>
-);
+  );
+};
 
 PropertyCard.propTypes = {
   title: PropTypes.string.isRequired,
